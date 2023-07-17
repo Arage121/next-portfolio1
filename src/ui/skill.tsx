41a9ff128a09431cf57acc5b087ea6c3728e1250
ayush <rajayush746@gmail.com>
2023-07-17 20:05:56 +0530

    finally last

--- src/ui/skill.tsx
+++ src/ui/skill.tsx
@@ -29,8 +29,8 @@ const Skill = ({ skills ,directionLeft }: Props) => {
     viewport={{ once: true }}
     className='w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center mt-24 p-8'>
        {
-        skills.map(({id, src, title, style})=>(
-          <div key={id} className={`shadow-md hover:scale-105 duration-500 px-10 sm:px-12 py-4 sm:py-2 xl:px-20 rounded-lg ${style}`}>
+        skills.map(({id, src, title})=>(
+          <div key={id} className={`shadow-md hover:scale-105 duration-500 px-10 sm:px-12 py-4 sm:py-2 xl:px-20 rounded-lg shadow-[#64ffda]/30`}>
             <img src={src} alt='' className='w-20 mx-auto sm:p-1'/>
             <p className='mt-3 text-[#dcfbf4] hidden sm:block'>{title}</p>
           </div>
